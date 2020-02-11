dce6fe75acae15aa690f52e7c7ceb972ae78f04c
brunoginciene <brunoginciene@hotmail.com>
2020-02-11 18:14:26 +0000

    Atualizar

--- js/create.js
+++ js/create.js
@@ -3,6 +3,11 @@ const db = require('./_database')
 async function createTables(){
     await db.connect()
 
+    await db.query(`CREATE TABLE table_1(
+        id serial PRIMARY KEY,
+        nome VARCHAR(50) UNIQUE NOT NULL
+    )`)
+
     await db.query(`CREATE TABLE table_2(
         id serial PRIMARY KEY,
         nome VARCHAR(50) UNIQUE NOT NULL
